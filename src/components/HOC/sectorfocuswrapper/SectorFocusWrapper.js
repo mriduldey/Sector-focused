087f0cbb3cae1564191a285386f6ab0fb0b06e8f
unknown <mriduldey94@gmail.com>
2021-07-23 09:43:57 +0530

    prop types added for hoc component

--- src/components/HOC/sectorfocuswrapper/SectorFocusWrapper.js
+++ src/components/HOC/sectorfocuswrapper/SectorFocusWrapper.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
 
 import "./SectorFocusWrapper.css";
 
-const SectorFocusWrapper = ({ FocusedWrapperName, children }) => {
+const SectorFocusWrapper = ({ focusedWrapperName, children }) => {
   return (
     <Grid container spacing={1} className="sector-focus-wrapper">
       <Grid item xs={12}>
-        <h1>{FocusedWrapperName}</h1>
+        <h1>{focusedWrapperName}</h1>
       </Grid>
       {children}
     </Grid>
   );
+};
+
+SectorFocusWrapper.propTypes = {
+  focusedWrapperName: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default SectorFocusWrapper;
